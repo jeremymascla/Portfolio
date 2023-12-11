77d423fd715e7efec99503a74a2f374301fe0ccf
jeremymascla <jeremy.mascla@gmail.com>
2023-12-11 13:49:29 +0100

    préparation send

---
+++ index.js
@@ -0,0 +1,20 @@
+const email=document.getElementById('email');
+const demande=document.getElementById('demande');
+const message=document.getElementById('message');
+const resend=document.getElementById('resend');
+const submit=document.querySelector('#submit');
+
+submit.addEventListener('submit',()=>{
+    // e.preventDefault();
+    console.log(`test `);
+
+    // Email.send({
+    //     SecureToken : "66091fc9-2432-4da6-8c54-0bafc913bc9c",
+    //     To : 'jeremy.mascla@gmail.com',
+    //     From : "jeremy.mascla@gmail.com",
+    //     Subject : "This is the subject",
+    //     Body : "And this is the body"
+    // }).then(
+    //   message => alert(message)
+    // );
+})
