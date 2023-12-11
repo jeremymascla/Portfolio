a1ce5ab97721dad792596b3d7609ef0a329e36ab
jeremymascla <jeremy.mascla@gmail.com>
2023-12-11 16:05:45 +0100

    fonctionnement envoie mail

--- index.js
+++ index.js
@@ -4,34 +4,17 @@ const message=document.querySelector('#message');
 const resend=document.querySelector('#resend');
 const submit=document.querySelector('#submit');
 
-submit.addEventListener('submit',()=>{
+submit.addEventListener('submit',(e)=>{
     e.preventDefault();
     console.log(`test`);
 
     Email.send({
         SecureToken : "66091fc9-2432-4da6-8c54-0bafc913bc9c",
-        To : 'jeremy.mascla@gmail.com',
+        To : `${email.value}`,
         From : "jeremy.mascla@gmail.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
+        Subject : `${demande.value}`,
+        Body : `de ${email.value} dit ${message.value}`
     }).then(
       message => alert(message)
-    );})
-
-// function envoie(){
-//     const email=document.querySelector('#email');
-//     const demande=document.querySelector('#demande');
-//     const message=document.querySelector('#message');
-//     const resend=document.querySelector('#resend');
-//     const submit=document.querySelector('#submit');
-
-//     console.log(`test ${email.value} ${demande.value} ${message.value} ${resend.checked}`);
-//     Email.send({
-//             SecureToken : "66091fc9-2432-4da6-8c54-0bafc913bc9c",
-//             To : 'jeremy.mascla@gmail.com',
-//             From : "jeremy.mascla@gmail.com",
-//             Subject : `${demande.value}`,
-//             Body : `${message.value}`
-//         }).then(
-//           message => alert(message)
-//         );}
+    );
+})
